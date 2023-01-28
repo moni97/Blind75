fac2257e7fb302287ca0a5648d4b4c484c1d5f40
moni97 <monisam97@gmail.com>
2023-01-27 20:35:12 -0600

    Create longestIncreasingSubsequence.py

---
+++ 1D Dynamin Programming/Longest Increasing Subsequence/longestIncreasingSubsequence.py
@@ -0,0 +1,10 @@
+def lengthOfLIS(self, nums: List[int]) -> int:
+    dp = [1] * len(nums)
+    dp[len(nums) - 1] = 1
+    res = 1
+    for i in range(len(nums)-2, -1, -1):
+        for j in range(i+1, len(nums)):
+            if nums[i] < nums[j]:
+                dp[i] = max(dp[i], 1+dp[j])
+                res = max(res, dp[i])
+    return res
