2df4aa5c9af3140a11de1b8ff9f1f8980ffe9b8b
moni97 <monisam97@gmail.com>
2023-02-04 15:23:00 -0600

    Update countingBits.py

--- Bit Manipulation/Counting Bits/countingBits.py
+++ Bit Manipulation/Counting Bits/countingBits.py
@@ -2,7 +2,7 @@ def countBits(self, n: int) -> List[int]:
         res = [0] * (n+1)
         offset = 1
         for i in range(1, n+1):
-            if i % 2 == 0:
+            if offset * 2 == i:
                 offset = i
             res[i] = 1 + res[i-offset]
         return res
